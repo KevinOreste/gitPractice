9a03d870f9001cc4349e37aec507007bd91ae886
KevinOreste <94265831+KevinOreste@users.noreply.github.com>
2023-05-23 20:24:34 -0300

    project initialization

---
+++ src/test/java/com/example/clasegit/ClasegitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.clasegit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClasegitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
